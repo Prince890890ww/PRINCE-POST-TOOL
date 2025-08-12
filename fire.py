1a2bf8f8c65f82c127f7f3e5ad62a46e17e5d9e2
Prince890890ww <159797414+Prince890890ww@users.noreply.github.com>
2025-08-12 15:24:36 +0530

    Update fire.py

--- fire.py
+++ fire.py
@@ -1,14 +1,1 @@
-#!/data/data/com.termux/files/usr/bin/python
-import platform,os
-import requests
-import bs4
-import faker
-import fake_email
-#####
-os.system("git pull")
-
-bit = platform.architecture()[0]
-if bit == '64bit':
-    import newwww
-elif bit == '32bit':
-    import newwww
+import bnaaaa
